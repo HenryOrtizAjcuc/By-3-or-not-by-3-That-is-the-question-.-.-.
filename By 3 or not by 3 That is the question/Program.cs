b3af6ee90d490903b3922309a840e26f3b8f0967
IT2 Henry <itprogintelaf2@outlook.com>
2020-11-24 07:47:36 -0600

    solution to by 3 or not by that is the question

---
+++ By 3 or not by 3 That is the question/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace By_3_or_not_by_3_That_is_the_question
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DivisibleByThree("123"));
+            Console.ReadKey();
+        }
+        public static bool DivisibleByThree(string n)
+        {
+            var lista = n.ToList().Sum(x => int.Parse(x.ToString())) % 3 == 0;
+            return lista;
+
+        }
+    }
+}
