fa3c91cef172bbc80a0c2727af874fcb0f3c26e4
HenryOrtizAjcuc <heoa100@gmail.com>
2020-11-24 08:28:47 -0600

    se agrego el archivo gitigonore

--- By 3 or not by 3 That is the question/Program.cs
+++ By 3 or not by 3 That is the question/Program.cs
@@ -10,9 +10,9 @@ namespace By_3_or_not_by_3_That_is_the_question
             Console.WriteLine(DivisibleByThree("123"));
             Console.ReadKey();
         }
-        public static bool DivisibleByThree(string n)
+        public static bool DivisibleByThree(string numero)
         {
-            var lista = n.ToList().Sum(x => int.Parse(x.ToString())) % 3 == 0;
+            var lista = numero.ToList().Sum(x => int.Parse(x.ToString())) % 3 == 0;
             return lista;
 
         }
